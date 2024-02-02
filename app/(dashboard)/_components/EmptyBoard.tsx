be7eecf165462ee849fa0bd379899a5a458ed3b4
Bhupesh <78196557+Bhupesh1x@users.noreply.github.com>
2024-02-02 17:53:32 +0530

    Create board api integration.

--- app/(dashboard)/_components/EmptyBoard.tsx
+++ app/(dashboard)/_components/EmptyBoard.tsx
@@ -1,8 +1,34 @@
+"use client";
+
+import { toast } from "sonner";
 import Image from "next/image";
+import { useOrganization } from "@clerk/nextjs";
+
+import { api } from "@/convex/_generated/api";
 
 import { Button } from "@/components/ui/button";
+import { useApiMutation } from "@/hooks/useApiMutation";
 
 function EmptyBoard() {
+  const { organization } = useOrganization();
+
+  const { mutate, pending } = useApiMutation(api.board.create);
+
+  const onClick = async () => {
+    if (!organization) return;
+
+    try {
+      const id = await mutate({
+        orgId: organization.id,
+        title: "Untitled",
+      });
+
+      toast.success("Board created");
+    } catch (error) {
+      toast.error("Failed to create board");
+    }
+  };
+
   return (
     <div className="h-full flex flex-col items-center justify-center">
       <Image src="/empty-board.svg" alt="Empty" width={110} height={110} />
@@ -13,7 +39,9 @@ function EmptyBoard() {
       </p>
 
       <div className="mt-6">
-        <Button size="lg">Create board</Button>
+        <Button size="lg" onClick={onClick} disabled={pending}>
+          Create board
+        </Button>
       </div>
     </div>
   );
