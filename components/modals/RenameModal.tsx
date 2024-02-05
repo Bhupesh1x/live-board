bd425d6768af4a92f7a8466cc570b01b82f7d5b2
Bhupesh <78196557+Bhupesh1x@users.noreply.github.com>
2024-02-05 18:18:10 +0530

    Finished rename modal changes and it's api integration.

--- components/modals/RenameModal.tsx
+++ components/modals/RenameModal.tsx
@@ -1,15 +1,48 @@
+import { toast } from "sonner";
+import { FormEventHandler, useEffect, useState } from "react";
+
+import { api } from "@/convex/_generated/api";
+import { useApiMutation } from "@/hooks/useApiMutation";
+import { useRenameModal } from "@/store/useRenameModal";
+
 import {
   Dialog,
+  DialogClose,
   DialogContent,
   DialogDescription,
   DialogFooter,
   DialogHeader,
   DialogTitle,
 } from "@/components/ui/dialog";
-import { useRenameModal } from "@/store/useRenameModal";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
 
 function RenameModal() {
   const { isOpen, onClose, initialValues } = useRenameModal();
+
+  const [title, setTitle] = useState(initialValues.title);
+
+  const { mutate, pending } = useApiMutation(api.board.update);
+
+  useEffect(() => {
+    setTitle(initialValues.title);
+  }, [initialValues.title]);
+
+  const onSubmit: FormEventHandler<HTMLFormElement> = async (e) => {
+    e.preventDefault();
+
+    try {
+      await mutate({
+        id: initialValues.id,
+        title,
+      });
+
+      toast.success("Board renamed");
+      onClose();
+    } catch (error) {
+      toast.error("Failed to rename board");
+    }
+  };
 
   return (
     <Dialog open={isOpen} onOpenChange={onClose}>
@@ -20,6 +53,28 @@ function RenameModal() {
             Enter a new title for this board
           </DialogDescription>
         </DialogHeader>
+
+        <form onSubmit={onSubmit} className="space-y-4">
+          <Input
+            required
+            value={title}
+            maxLength={60}
+            disabled={pending}
+            placeholder="Board Title"
+            onChange={(e) => setTitle(e.target.value)}
+          />
+
+          <DialogFooter>
+            <DialogClose asChild>
+              <Button variant="outline" type="button">
+                Cancel
+              </Button>
+            </DialogClose>
+            <Button disabled={pending} type="submit">
+              Save
+            </Button>
+          </DialogFooter>
+        </form>
       </DialogContent>
     </Dialog>
   );
