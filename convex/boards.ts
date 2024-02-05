083fc572378ae6ceba8848690dffb6b4f68c395a
Bhupesh <78196557+Bhupesh1x@users.noreply.github.com>
2024-02-05 20:20:21 +0530

    Updated boards call to add isFavorite value.

--- convex/boards.ts
+++ convex/boards.ts
@@ -18,6 +18,23 @@ export const get = query({
       .order("desc")
       .collect();
 
-    return boards;
+    const boardsWithFavorite = boards.map((board) => {
+      return ctx.db
+        .query("userFavorites")
+        .withIndex("by_user_board", (q) =>
+          q.eq("userId", identity.subject).eq("boardId", board._id)
+        )
+        .unique()
+        .then((favorite) => {
+          return {
+            ...board,
+            isFavorite: !!favorite,
+          };
+        });
+    });
+
+    const boardsWithFavoriteBoolean = Promise.all(boardsWithFavorite);
+
+    return boardsWithFavoriteBoolean;
   },
 });
