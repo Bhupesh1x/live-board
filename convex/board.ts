bd6fdb9de3557d2d2eb80af0f9435e47657100d8
Bhupesh <78196557+Bhupesh1x@users.noreply.github.com>
2024-02-05 20:30:39 +0530

    Updated backend delete function to delete the board details from the user favorite as well if user delete the board.

--- convex/board.ts
+++ convex/board.ts
@@ -52,6 +52,17 @@ export const remove = mutation({
       throw new Error("User unauthorized");
     }
 
+    const existingFavorite = await ctx.db
+      .query("userFavorites")
+      .withIndex("by_user_board", (q) =>
+        q.eq("userId", identity.subject).eq("boardId", args.id)
+      )
+      .unique();
+
+    if (existingFavorite) {
+      await ctx.db.delete(existingFavorite._id);
+    }
+
     await ctx.db.delete(args.id);
   },
 });
