9d76ae9b34676568b65ae6ba57e8d1db376322f0
Bhupesh <78196557+Bhupesh1x@users.noreply.github.com>
2024-02-07 20:29:34 +0530

    Frontend updated the participants component.

--- app/(board)/board/[boardId]/_components/Participants.tsx
+++ app/(board)/board/[boardId]/_components/Participants.tsx
@@ -1,7 +1,44 @@
+"use client";
+
+import { connectionIdToColor } from "@/lib/utils";
+import { useSelf, useOthers } from "@/liveblocks.config";
+
+import UserAvatar from "./UserAvatar";
+
+const MAX_SHOWN_USERS = 2;
+
 function Participants() {
+  const users = useOthers();
+  const currUser = useSelf();
+  const hasMoreUsers = users.length > MAX_SHOWN_USERS;
+
   return (
     <div className="absolute top-2 right-2 p-3 h-12 rounded-md shadow-md flex items-center bg-white">
-      <h1>List of the users</h1>
+      <div className="flex gap-2">
+        {currUser && (
+          <UserAvatar
+            src={currUser?.info?.picture}
+            name={`${currUser?.info?.name} (You)`}
+            fallback={currUser?.info?.name?.[0]}
+            borderColor={connectionIdToColor(currUser.connectionId)}
+          />
+        )}
+        {users?.slice(0, MAX_SHOWN_USERS)?.map(({ connectionId, info }) => (
+          <UserAvatar
+            key={connectionId}
+            src={info?.picture}
+            name={info?.name}
+            fallback={info?.name?.[0] || "T"}
+            borderColor={connectionIdToColor(connectionId)}
+          />
+        ))}
+        {hasMoreUsers && (
+          <UserAvatar
+            name={`${users.length - MAX_SHOWN_USERS} more`}
+            fallback={`+${users.length - MAX_SHOWN_USERS}`}
+          />
+        )}
+      </div>
     </div>
   );
 }
