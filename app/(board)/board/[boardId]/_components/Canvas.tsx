97dd64f84723b24c527bb14443f5bf886a116cb2
Bhupesh <78196557+Bhupesh1x@users.noreply.github.com>
2024-02-07 19:23:03 +0530

    Frontend updated the info component in the board id page

--- app/(board)/board/[boardId]/_components/Canvas.tsx
+++ app/(board)/board/[boardId]/_components/Canvas.tsx
@@ -4,18 +4,14 @@ import Info from "./Info";
 import Toolbar from "./Toolbar";
 import Participants from "./Participants";
 
-import { useSelf } from "@/liveblocks.config";
-
 type Props = {
   boardId: string;
 };
 
 function Canvas({ boardId }: Props) {
-  const info = useSelf((me) => me.info);
-
   return (
     <main className="h-full w-full relative bg-neutral-100 touch-none">
-      <Info />
+      <Info boardId={boardId} />
       <Participants />
       <Toolbar />
     </main>
--- app/(board)/board/[boardId]/_components/Info.tsx
+++ app/(board)/board/[boardId]/_components/Info.tsx
@@ -1,7 +1,70 @@
-function Info() {
+"use client";
+
+import Link from "next/link";
+import Image from "next/image";
+import { Menu } from "lucide-react";
+import { useQuery } from "convex/react";
+import { Poppins } from "next/font/google";
+
+import { api } from "@/convex/_generated/api";
+import { Id } from "@/convex/_generated/dataModel";
+import { useRenameModal } from "@/store/useRenameModal";
+
+import Hint from "@/components/shared/Hint";
+import { Button } from "@/components/ui/button";
+import Actions from "@/components/shared/Actions";
+
+type Props = {
+  boardId: string;
+};
+
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["600"],
+});
+
+function Info({ boardId }: Props) {
+  const board = useQuery(api.board.get, { id: boardId as Id<"boards"> });
+
+  const { onOpen } = useRenameModal();
+
+  if (!board) return <InfoSkeleton />;
+
   return (
     <div className="absolute top-2 left-2 px-1.5 h-12 rounded-md shadow-md flex items-center bg-white">
-      Information about the board
+      <Hint label="Go to boards" side="bottom" sideOffset={10}>
+        <Button asChild variant="board" className="px-2">
+          <Link href="/">
+            <Image src="/logo.svg" alt="Logo" height={30} width={30} />
+            <span
+              className={`text-xl font-semibold ml-2 text-black ${font.className}`}
+            >
+              LiveBoard
+            </span>
+          </Link>
+        </Button>
+      </Hint>
+      <div className="text-neutral-400 px-1.5">|</div>
+      <Hint label="Edit title" side="bottom" sideOffset={10}>
+        <Button
+          variant="board"
+          className="text-base font-normal px-2"
+          onClick={() => onOpen(board._id, board.title)}
+        >
+          {board.title}
+        </Button>
+      </Hint>
+      <div className="text-neutral-400 px-1.5">|</div>
+
+      <Actions id={board._id} title={board.title} side="bottom" sideOffset={10}>
+        <div>
+          <Hint label="Main menu" side="bottom" sideOffset={10}>
+            <Button size="icon" variant="board">
+              <Menu />
+            </Button>
+          </Hint>
+        </div>
+      </Actions>
     </div>
   );
 }
