3dcfa83d183f391d1304ed782b606247f2df75a0
Bhupesh <78196557+Bhupesh1x@users.noreply.github.com>
2024-02-08 19:01:56 +0530

    Frontend added the additional types for the convex state and onClick and isActive changes for the ToolButton component.

--- types/canvas.ts
+++ types/canvas.ts
@@ -1,7 +1,132 @@
-export type CanvasState = {
-  mode: CanvasMode.None;
-};
-
 export enum CanvasMode {
   None,
+  Pressing,
+  SelectionNet,
+  Translating,
+  Inserting,
+  Resizing,
+  Pencil,
 }
+
+export type Color = {
+  r: number;
+  g: number;
+  b: number;
+};
+
+export type Camera = {
+  x: number;
+  y: number;
+};
+
+export enum LayerType {
+  Rectangle,
+  Ellipse,
+  Path,
+  Text,
+  Note,
+}
+
+export type RectangleLayer = {
+  type: LayerType.Rectangle;
+  height: number;
+  width: number;
+  x: number;
+  y: number;
+  fill: Color;
+  value?: string;
+};
+
+export type EllipseLayer = {
+  type: LayerType.Ellipse;
+  height: number;
+  width: number;
+  x: number;
+  y: number;
+  fill: Color;
+  value?: string;
+};
+
+export type PathLayer = {
+  type: LayerType.Ellipse;
+  height: number;
+  width: number;
+  x: number;
+  y: number;
+  fill: Color;
+  points: number[][];
+  value?: string;
+};
+
+export type TextLayer = {
+  type: LayerType.Text;
+  height: number;
+  width: number;
+  x: number;
+  y: number;
+  fill: Color;
+  value?: string;
+};
+
+export type NoteLayer = {
+  type: LayerType.Note;
+  height: number;
+  width: number;
+  x: number;
+  y: number;
+  fill: Color;
+  value?: string;
+};
+
+export type Point = {
+  x: number;
+  y: number;
+};
+
+export type XYWH = {
+  x: number;
+  y: number;
+  height: number;
+  width: number;
+};
+
+export enum Side {
+  Top = 1,
+  Bottom = 2,
+  LEft = 4,
+  Right = 8,
+}
+
+export type CanvasState =
+  | {
+      mode: CanvasMode.None;
+    }
+  | {
+      mode: CanvasMode.Pressing;
+      origin: Point;
+    }
+  | {
+      mode: CanvasMode.SelectionNet;
+      origin: Point;
+      current?: Point;
+    }
+  | {
+      mode: CanvasMode.Translating;
+      current: Point;
+    }
+  | {
+      mode: CanvasMode.Inserting;
+      layerType:
+        | LayerType.Ellipse
+        | LayerType.Note
+        | LayerType.Rectangle
+        | LayerType.Text;
+    }
+  | {
+      mode: CanvasMode.Resizing;
+      initialBounds: XYWH;
+      cornor: Side;
+    }
+  | {
+      mode: CanvasMode.Pencil;
+    };
